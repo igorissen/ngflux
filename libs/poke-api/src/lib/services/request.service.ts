c67d52b39d1a20a419ba4f553d64c46341d18cf6
Ismael Gorissen <ismael.gorissen@gmail.com>
2022-04-19 22:09:20 +0200

    refactor(poke-api): add new argument for RequestService class

--- libs/poke-api/src/lib/services/request.service.ts
+++ libs/poke-api/src/lib/services/request.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { PokeApiConfiguration } from '../models/poke-api-configuration';
 
 @Injectable({ providedIn: 'root' })
 export class RequestService {
-  constructor(private readonly _httpClient: HttpClient) {}
+  constructor(
+    private readonly _httpClient: HttpClient,
+    private readonly _configuration: PokeApiConfiguration
+  ) {}
 }
--- libs/poke-api/src/lib/types/index.ts
+++ libs/poke-api/src/lib/types/index.ts
@@ -1,4 +1,32 @@
-export type NgxPokeApiOptions = {
+export type Berry = {
+  firmness: NamedApiResource;
+  flavors: BerryFlavor[];
+  growth_time: number;
+  id: number;
+  item: NamedApiResource;
+  max_harvest: number;
+  name: string;
+  natural_gift_power: number;
+  natural_gift_type: NamedApiResource;
+  size: number;
+  smoothness: number;
+  soil_dryness: number;
+};
+export type BerryFlavor = {
+  potency: number;
+  flavor: NamedApiResource;
+};
+export type NamedApiResource = {
+  name: string;
+  url: string;
+};
+export type NamedApiResourceList = {
+  count: number;
+  next: string | null | undefined;
+  previous: string | null | undefined;
+  results: NamedApiResource[];
+};
+export type PokeApiOptions = {
   baseUrl?: string;
   version?: string;
 };
